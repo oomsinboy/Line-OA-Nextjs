5b0d696c8a95a44c40738c0c4ab2b0c2676a908c
Payupat <payupat@planetcloud.cloud>
2024-06-26 17:47:24 +0700

    fix patient view

--- src/app/home/patient/view-patient/page.tsx
+++ src/app/home/patient/view-patient/page.tsx
@@ -1,19 +1,32 @@
 "use client"
 
 import Navbar from '@/components/Navbar';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import PatientstateID from '@/components/patient/PatientStateid';
 
 function ViewPatient() {
-    const searchParams = useSearchParams()
-    const id = searchParams.get('id')
-    const state = searchParams.get('state')
+    const searchParams = useSearchParams();
+    const [id, setId] = useState<number>(0);
+    const [state, setState] = useState<string>('');
+
+    useEffect(() => {
+        const paramId = searchParams.get('id');
+        const paramState = searchParams.get('state');
+        
+        // แปลงค่า id และตรวจสอบให้แน่ใจว่าไม่ใช่ null
+        if (paramId) {
+            setId(parseInt(paramId));
+        }
+        if (paramState) {
+            setState(paramState);
+        }
+    }, [searchParams]);
 
     return (
         <div className='min-h-screen'>
             <Navbar />
-            <PatientstateID id={patientId} state={state} />
+            <PatientstateID id={id} state={state} />
         </div>
     )
 
